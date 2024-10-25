3ca127a25642d3f525c0979ac7cbaacf06064e4c
Ivanhoe Estrera <ivanhoeestrera1998@gmail.com>
2024-10-25 19:05:02 +0800

    Added AddProduct Component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddProduct from './AddProduct';
+
+function App() {
+    return (
+        <div>
+            <AddProduct />
+        </div>
+    );
+}
+
+export default App;
